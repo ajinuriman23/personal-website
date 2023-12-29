05a61ddcaa8d75d402982f5113016ea80f963704
Aji Nur Iman <ajinuriman230203@gmail.com>
2023-12-29 15:25:26 +0700

    Update commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
  
 import { ThemeProvider } from "@material-tailwind/react";
  
